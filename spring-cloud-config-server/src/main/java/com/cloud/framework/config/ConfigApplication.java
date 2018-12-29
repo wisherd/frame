163451f7e15b3bf81bdb70ea9a92cf94ee3ca125
liuwei <liuwei@uto360.com>
2018-12-29 15:48:14 +0800

    新增配置

--- spring-cloud-config-server/src/main/java/com/cloud/framework/config/ConfigApplication.java
+++ spring-cloud-config-server/src/main/java/com/cloud/framework/config/ConfigApplication.java
@@ -1,13 +1,9 @@
 package com.cloud.framework.config;
 
-import com.cloud.util.LoggerUtils;
-import com.cloud.util.TLogger;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.ComponentScan;
 
 
 /**
@@ -23,10 +19,8 @@ import org.springframework.context.annotation.ComponentScan;
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
-        DB_LOGGER.warn("ConfigApplication started successfully");
     }
 
-    private static final transient TLogger DB_LOGGER = LoggerUtils.getLogger(ConfigApplication.class);
 
 
 }
